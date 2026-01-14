e15fa20a47f09e8b4d784a4b19bf2773701b4d37
oppa073-hub <oppa073@gmail.com>
2026-01-14 11:01:47 +0900

    <feat> 퍼즐 커맨드 실행기 구현

---
+++ Assets/2. Manager/CommandProcessor.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun.Demo.PunBasics;
+using UnityEngine;
+
+public class CommandProcessor : MonoBehaviour
+{
+    public static CommandProcessor Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    [SerializeField] playerController player;
+
+    private void OnEnable() => player.OnInteract += Execute;
+    private void OnDisable() => player.OnInteract -= Execute;
+
+    private void Execute(ICommand cmd) => cmd.Execute();
+
+
+}
