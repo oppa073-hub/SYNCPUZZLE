8eb62fdf2b311ffad790cad6508ca741fb6138f5
oppa073-hub <oppa073@gmail.com>
2026-01-17 22:40:42 +0900

    <feat> 비밀번호퍼즐 스크립트 생성 + UI매니저 초기설계

---
+++ Assets/2. Manager/UIManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+    /*
+     * OpenKeypad(int puzzleId) : 패널 켜고 puzzleId 저장
+
+       CloseKeypad() : 패널 끄기
+       
+       OnKeypadNumber(int n) : RequestPress(puzzleId, action=0, value=n)
+       
+       OnKeypadBack() : RequestPress(puzzleId, action=1, value=0)
+       
+       OnKeypadClear() : RequestPress(puzzleId, action=2, value=0)
+       
+       OnKeypadEnter() : RequestPress(puzzleId, action=3, value=0)
+     * 
+     */
+
+    [SerializeField] GameObject KeypadPanel;
+    [SerializeField] 
+    private void Start()
+    {
+        KeypadPanel.SetActive(false);
+    }
+
+    public void OpenKeyPad()
+    {
+        KeypadPanel.SetActive(true);
+    }
+    public void CloseKeyPad()
+    {
+        KeypadPanel.SetActive(false);
+    }
+    public void OnKeypadNumber(int n)
+    {
+        PuzzleManager.Instance.RequestPress(3, 0, n);
+    }
+    public void OnKeypadEnter()
+    {
+        PuzzleManager.Instance.RequestPress(3, 1, 0);
+    }
+
+
+}
